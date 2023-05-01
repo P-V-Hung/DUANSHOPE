1c2a653cf2e397fcf4d01739a2d713ce42385409
P-V-Hung <azhung08102004@gmail.com>
2023-05-01 15:39:54 +0700

    js log-in form

--- javascript/eyepassword.js
+++ javascript/eyepassword.js
@@ -1,3 +1,4 @@
+// eyepassword
 const eyepassword = document.querySelectorAll(".auth-form_input-password");
 const icon_password = document.querySelectorAll(".fa-eye");
 let error_password = document.querySelector('.error_password');
@@ -9,6 +10,24 @@ for(let i = 0; i < icon_password.length ;i++){
         }else{
             eyepassword[i].type = "text";
         }
+    }
+}
+//Form log
+let content_form = document.getElementsByClassName('black-cover');
+const bong = document.getElementsByClassName('black-cover-bong');
+let dk = document.getElementsByClassName('header__nav-iteam-link-bold');
+let auth_form = document.getElementsByClassName('auth-form');
+for(let i = 0;i <dk.length;i++){
+    dk[i].onclick = function (){
+        content_form[0].style.display = "flex";
+        auth_form[i].style.display = "block";
+    }
+
+}
+bong[0].onclick = function(){
+    content_form[0].style.display = "none";
+    for(let i=0;i<dk.length;i++){
+        auth_form[i].style.display = "none";
     }
 }
 
