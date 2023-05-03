f52e4332b479d0bde78ba8884f63b69c98bbc507
P-V-Hung <azhung08102004@gmail.com>
2023-05-03 15:25:24 +0700

    hoan thien header

--- javascript/eyepassword.js
+++ javascript/eyepassword.js
@@ -1,33 +1,48 @@
 // eyepassword
 const eyepassword = document.querySelectorAll(".auth-form_input-password");
 const icon_password = document.querySelectorAll(".fa-eye");
-let error_password = document.querySelector('.error_password');
-let password_dk = "";
+let password_icon = document.querySelectorAll('.fa-solid.fa-eye');
+console.log(password_icon);
 for(let i = 0; i < icon_password.length ;i++){
     icon_password[i].onclick = function (){
         if(eyepassword[i].type == "text"){
             eyepassword[i].type = "password";
+            password_icon[i].classList.replace('fa-eye-slash','fa-eye')
         }else{
             eyepassword[i].type = "text";
+            password_icon[i].classList.replace('fa-eye','fa-eye-slash')
         }
     }
 }
+{/* <i class="fa-solid fa-eye-slash"></i> */}
 //Form log
 let content_form = document.getElementsByClassName('black-cover');
-const bong = document.getElementsByClassName('black-cover-bong');
-let dk = document.getElementsByClassName('header__nav-iteam-link-bold');
+let tl = document.getElementsByClassName('btn auth-form_control-back');
+let dkdn = document.getElementsByClassName('header__nav-iteam-link-bold');
 let auth_form = document.getElementsByClassName('auth-form');
-for(let i = 0;i <dk.length;i++){
-    dk[i].onclick = function (){
+for(let i = 0;i <dkdn.length;i++){
+    dkdn[i].onclick = function (){
         content_form[0].style.display = "flex";
         auth_form[i].style.display = "block";
     }
-
-}
-bong[0].onclick = function(){
-    content_form[0].style.display = "none";
-    for(let i=0;i<dk.length;i++){
+    tl[i].onclick = function (){
+        content_form[0].style.display = "none";
         auth_form[i].style.display = "none";
+    }
+}
+
+
+//Log-in 
+let dk = document.querySelectorAll(".auth-form__switch-btn");
+for(let i =0;i < dk.length;i++) {
+    dk[i].onclick = function () {
+        auth_form[i].style.display = "none";
+        if(i==0){
+            auth_form[1].style.display = "block";
+        }
+        if(i==1){
+            auth_form[0].style.display = "block";
+        }
     }
 }
 
